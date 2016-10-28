2d3e9beaa4849c6972ed71bf95248ed70d7e0f3c
rowaasr13 <10935437+rowaasr13@users.noreply.github.com>
2016-10-28 11:13:25 +0300

    LibToast + chat outputs

--- LDB-WoWToken.lua
+++ LDB-WoWToken.lua
@@ -16,12 +16,21 @@ local history_cutout = history_length + 2
 
 local qtip = LibStub("LibQTip-1.0")
 local ldb = LibStub:GetLibrary("LibDataBroker-1.1")
+local LibToast = LibStub("LibToast-1.0", 1)
 
 local dataobj = ldb:NewDataObject(addon_name, {
    label = addon_name,
    type = "data source",
    icon = "Interface\\Icons\\WoW_Token01",
 })
+
+if LibToast then
+   -- /run LibStub("LibToast-1.0"):Spawn("LDB-WoWToken", 123, 456)
+   LibToast:Register(addon_name, function(toast, new_price, diff)
+      toast:SetFormattedTitle("%s", addon_name)
+      toast:SetFormattedText("%s %s", new_price, (diff or ""))
+   end)
+end
 
 local history_timestamp = {}
 local history_price = {}
@@ -70,7 +79,10 @@ local function OnTokenPriceUpdate(self, event, result)
       if history_price[2] then
          diff = DiffString(history_price[1], history_price[2])
       end
-      dataobj.text = GetMoneyString(current_price, true) .. " " .. (diff or "")
+      local money_string = GetMoneyString(current_price, true)
+      dataobj.text = money_string .. " " .. (diff or "")
+      if LibToast then LibToast:Spawn(addon_name, money_string, diff) end
+      print(addon_name .. ": " .. money_string .. " " .. (diff or ""))
    else
       dataobj.text = "N/A"
    end
