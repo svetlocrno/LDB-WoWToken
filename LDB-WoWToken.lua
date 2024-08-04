11797eb4674c1f127dec9a7edad852c1138b9ca3
svetlocrno <42773112+svetlocrno@users.noreply.github.com>
2024-08-04 02:11:19 +0200

    Update LDB-WoWToken.lua

--- LDB-WoWToken.lua
+++ LDB-WoWToken.lua
@@ -1,5 +1,7 @@
 local a_name, a_env = ...
-if not a_env.load_this then return end
+if not a_env.load_this then
+	return
+end
 
 local sv_name = "SV_LDBWoWToken"
 
@@ -22,24 +24,30 @@ local ldb = LibStub:GetLibrary("LibDataBroker-1.1")
 local LibToast = LibStub("LibToast-1.0", 1)
 
 local dataobj = ldb:NewDataObject(a_name, {
-   label = a_name,
-   type = "data source",
-   icon = "Interface\\Icons\\WoW_Token01",
+	label = a_name,
+	type = "data source",
+	icon = "Interface\\Icons\\WoW_Token01",
 })
 
 if LibToast then
-   -- /run LibStub("LibToast-1.0"):Spawn("LDB-WoWToken", 123, 456)
-   LibToast:Register(a_name, function(toast, new_price, diff)
-      toast:SetFormattedTitle("%s", a_name)
-      toast:SetFormattedText("%s %s", new_price, (diff or ""))
-   end)
+	-- /run LibStub("LibToast-1.0"):Spawn("LDB-WoWToken", 123, 456)
+	LibToast:Register(a_name, function(toast, new_price, diff)
+		toast:SetFormattedTitle("%s", a_name)
+		toast:SetFormattedText("%s %s", new_price, (diff or ""))
+	end)
 end
 
 local function InitSV(sv_name)
-   if not _G[sv_name]                   then _G[sv_name] = {} end
+	if not _G[sv_name] then
+		_G[sv_name] = {}
+	end
 
-   if not _G[sv_name].history_timestamp then _G[sv_name].history_timestamp = {} end
-   if not _G[sv_name].history_price     then _G[sv_name].history_price = {} end
+	if not _G[sv_name].history_timestamp then
+		_G[sv_name].history_timestamp = {}
+	end
+	if not _G[sv_name].history_price then
+		_G[sv_name].history_price = {}
+	end
 end
 InitSV(sv_name)
 
@@ -48,115 +56,141 @@ local current_h2
 
 local event_frame = CreateFrame("Frame")
 
-local function DiffString(now, before)
-   local swap = SV_LDBWoWToken.diff_colors == "seller"
+local function comma_value(n)
+	local left, num, right = string.match(n, "^([^%d]*%d)(%d*)(.-)$")
+	return left .. (num:reverse():gsub("(%d%d%d)", "%1,"):reverse()) .. right
+end
 
-   local diff = now - before
-   local color, sign
-   if diff < 0 then
-      color = swap and RED_FONT_COLOR_CODE or GREEN_FONT_COLOR_CODE
-      sign = "-"
-      diff = -diff
-   else
-      color = swap and GREEN_FONT_COLOR_CODE or RED_FONT_COLOR_CODE
-      sign = "+"
-   end
-   return color .. sign .. GetMoneyString(diff, true)
+local function DiffString(now, before)
+	local swap = SV_LDBWoWToken.diff_colors == "seller"
+
+	local diff = now - before
+	local color, sign
+	if diff < 0 then
+		color = swap and RED_FONT_COLOR_CODE or GREEN_FONT_COLOR_CODE
+		sign = "-"
+		diff = -diff / before * 100
+	else
+		color = swap and GREEN_FONT_COLOR_CODE or RED_FONT_COLOR_CODE
+		sign = "+"
+		diff = diff / before * 100
+	end
+
+	if diff < 1 then
+		return color .. sign .. comma_value(diff / 1000000 * before) .. "|cfff0c80bg|r"
+	else
+		return color .. sign .. tonumber(string.format("%.1f", diff)) .. "%"
+	end
 end
 
 local function OnTokenPriceUpdate(self, event, result)
-   local sv = _G[sv_name]
-   local history_timestamp = sv.history_timestamp
-   local history_price     = sv.history_price
+	local sv = _G[sv_name]
+	local history_timestamp = sv.history_timestamp
+	local history_price = sv.history_price
 
-   local new_price
-   if event == "_MANUAL_UPDATE" then
-      new_price = result
-   else
-      if result ~= LE_TOKEN_RESULT_SUCCESS then new_price = nil else new_price = GetCurrentMarketPrice() end
-   end
+	local new_price
+	if event == "_MANUAL_UPDATE" then
+		new_price = result
+	else
+		if result ~= LE_TOKEN_RESULT_SUCCESS then
+			new_price = nil
+		else
+			new_price = GetCurrentMarketPrice()
+		end
+	end
 
-   if new_price == current_price and current_h2 == history_price[2] then return end
+	if new_price == current_price and current_h2 == history_price[2] then
+		return
+	end
 
-   current_price = new_price
-   if current_price then
-      if current_price ~= history_price[1] then
-         tinsert(history_timestamp, 1, time())
-         tinsert(history_price, 1, current_price)
-         history_timestamp[history_cutout] = nil
-         history_price[history_cutout] = nil
-      end
-      local diff
-      if history_price[2] then
-         diff = DiffString(history_price[1], history_price[2])
-      end
-      local money_string = GetMoneyString(current_price, true)
-      dataobj.text = money_string .. " " .. (diff or "")
-      if LibToast and SV_LDBWoWToken.diff_libtoast ~= false then LibToast:Spawn(a_name, money_string, diff) end
-      if SV_LDBWoWToken.diff_chat ~= false then print(a_name .. ": " .. money_string .. " " .. (diff or "")) end
-   else
-      dataobj.text = "N/A"
-   end
-   current_h2 = history_price[2]
+	current_price = new_price
+	if current_price then
+		if current_price ~= history_price[1] then
+			tinsert(history_timestamp, 1, time())
+			tinsert(history_price, 1, current_price)
+			history_timestamp[history_cutout] = nil
+			history_price[history_cutout] = nil
+		end
+		local diff
+		if history_price[2] then
+			diff = DiffString(history_price[1], history_price[2])
+		end
+
+		local money_string = comma_value(current_price / 10000) .. "|cfff0c80bg|r"
+
+		dataobj.text = money_string .. " " .. (diff or "")
+		if LibToast and SV_LDBWoWToken.diff_libtoast ~= false then
+			LibToast:Spawn(a_name, money_string, diff)
+		end
+		if SV_LDBWoWToken.diff_chat ~= false then
+			print(a_name .. ": " .. money_string .. " " .. (diff or ""))
+		end
+	else
+		dataobj.text = "N/A"
+	end
+	current_h2 = history_price[2]
 end
 
 local function OnLoaded(self)
-   local sv = _G[sv_name]
-   local history_price     = sv.history_price
+	local sv = _G[sv_name]
+	local history_price = sv.history_price
 
-   local pre_load_price = current_price
-   current_price = history_price[1]
-   OnTokenPriceUpdate(self, "_MANUAL_UPDATE", pre_load_price)
-   event_frame:SetScript("OnEvent", OnTokenPriceUpdate)
-   event_frame:UnregisterEvent("ADDON_LOADED")
-   -- Literally WTF? After wouldn't take built-in C_WowTokenPublic.UpdateMarketPrice as argument. Is that a quirk of new C-side After?
-   After(1, function() UpdateMarketPrice() end)
+	local pre_load_price = current_price
+	current_price = history_price[1]
+	OnTokenPriceUpdate(self, "_MANUAL_UPDATE", pre_load_price)
+	event_frame:SetScript("OnEvent", OnTokenPriceUpdate)
+	event_frame:UnregisterEvent("ADDON_LOADED")
+	-- Literally WTF? After wouldn't take built-in C_WowTokenPublic.UpdateMarketPrice as argument. Is that a quirk of new C-side After?
+	After(1, function()
+		UpdateMarketPrice()
+	end)
 end
 
 event_frame:RegisterEvent("TOKEN_MARKET_PRICE_UPDATED")
 event_frame:RegisterEvent("ADDON_LOADED")
 event_frame:SetScript("OnEvent", function(self, event, result)
-   if event == "ADDON_LOADED" then
-      if result == a_name then
-         OnLoaded(self)
-      end
-   else
-      OnTokenPriceUpdate(self, event, result)
-   end
+	if event == "ADDON_LOADED" then
+		if result == a_name then
+			OnLoaded(self)
+		end
+	else
+		OnTokenPriceUpdate(self, event, result)
+	end
 end)
 
 local function UpatePriceAndReschedule()
-   UpdateMarketPrice()
-   After(60, UpatePriceAndReschedule)
+	UpdateMarketPrice()
+	After(60, UpatePriceAndReschedule)
 end
 After(1, UpatePriceAndReschedule)
 
 local tooltip
 
 function dataobj:OnEnter()
-   local sv = _G[sv_name]
-   local history_timestamp = sv.history_timestamp
-   local history_price     = sv.history_price
+	local sv = _G[sv_name]
+	local history_timestamp = sv.history_timestamp
+	local history_price = sv.history_price
 
-   tooltip = qtip:Acquire(a_name, 3, "LEFT", "RIGHT", "RIGHT")
-   tooltip:AddHeader("Time", "Price", "Diff")
-   for idx = 1, history_length do
-      local price = history_price[idx]
-      local diff
-      if price then
-         local older_price = history_price[idx + 1]
-         if older_price then
-            diff = DiffString(price, older_price)
-         end
-         price = GetMoneyString(price, true) or ""
-         tooltip:AddLine(date("%Y/%m/%d (%a) %H:%M", history_timestamp[idx]), price, diff or "")
-      end
-   end
-   tooltip:SmartAnchorTo(self)
-   tooltip:Show()
+	tooltip = qtip:Acquire(a_name, 3, "LEFT", "RIGHT", "RIGHT")
+	tooltip:AddHeader("Time", "Price", "Diff")
+
+	for idx = history_length, 1, -1 do
+		local price = history_price[idx]
+		local diff
+		if price then
+			local older_price = history_price[idx + 1]
+			if older_price then
+				diff = DiffString(price, older_price)
+			end
+			price = comma_value(price / 10000) .. "|cfff0c80bg|r" or ""
+			tooltip:AddLine(date("%Y/%m/%d (%a) %H:%M", history_timestamp[idx]), price, diff or "")
+		end
+	end
+	tooltip:SmartAnchorTo(self)
+	tooltip:Show()
 end
 
 function dataobj:OnLeave()
-   qtip:Release(tooltip)
-   tooltip = nil
+	qtip:Release(tooltip)
+	tooltip = nil
 end
